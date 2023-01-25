088346428ba1d729f721c648cd0cc15df8f7ab6d
kaanersoy <kaanersoy12@gmail.com>
2023-01-25 19:36:59 +0300

    init client project

---
+++ client/.eslintrc.cjs
@@ -0,0 +1,34 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:react/recommended',
+    'airbnb',
+    'airbnb-typescript',
+  ],
+  plugins: [
+    'jsx-a11y',
+    '@typescript-eslint',
+    'import',
+  ],
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    },
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './client/tsconfig.json',
+  },
+  rules: {
+    'react/jsx-filename-extension': [2, { extensions: ['.ts', '.tsx'] }],
+  },
+};
