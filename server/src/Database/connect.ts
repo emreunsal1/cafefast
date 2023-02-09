baab12561013eba2685b2b2b01df51187a2fe5f2
kaanersoy <kaanersoy12@gmail.com>
2023-02-09 19:15:32 +0300

    update connect database file

--- server/src/Database/connect.ts
+++ server/src/Database/connect.ts
@@ -1,14 +1,16 @@
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 
-dotenv.config();
-const { DB_URL } = process.env;
-mongoose.set("strictQuery", true);
-
-const connectDB = () => {
-  mongoose.connect(DB_URL as string).then(() => {
+const connectDB = async () => {
+  dotenv.config();
+  const { DB_URL } = process.env;
+  mongoose.set("strictQuery", true);
+  try {
+    await mongoose.connect(DB_URL as string);
     console.log("Connected to database");
-  });
+  } catch (err) {
+    console.log("Error while connecting to database => ", err);
+  }
 };
 
 export default connectDB;
