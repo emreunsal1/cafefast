bb9f6f2b2fb7c5e9a87da3c898437ce7504ebe16
Emre Ünsal <unsalemre111@gmail.com>
2023-07-18 21:34:15 +0300

    add payment page and first order compeleted yupi brah

---
+++ menu/src/pages/payment.jsx
@@ -0,0 +1,62 @@
+import React, { useEffect, useState } from "react";
+import Cards from "react-credit-cards";
+import { useBasket } from "@/context/Basket";
+import BASKET_SERVICE from "@/services/basket";
+import "react-credit-cards/es/styles-compiled.css";
+
+export default function Payment() {
+  const [card, setCard] = useState({
+    cvc: "",
+    expiry: "",
+    focus: "name",
+    name: "",
+    number: "",
+  });
+
+  const { getBasketItems, basketItems } = useBasket();
+
+  useEffect(() => {
+    getBasketItems({ companyId: "64208d2c890cdcf8376c87a5" });
+  }, []);
+
+  const handleInputFocus = (key) => {
+    setCard((prev) => ({ ...prev, focus: key }));
+  };
+  const inputOnChangeHandler = (event) => {
+    event.preventDefault();
+    const { name, value } = event.target;
+    setCard((prev) => ({ ...prev, [name]: value }));
+  };
+
+  const formSubmitHandler = (e) => {
+    e.preventDefault();
+    const cardData = {
+      cardNo: card.number,
+      cvv: card.cvc,
+      thruMonth: card.expiry.slice(0, 2),
+      thruYear: card.expiry.slice(2, 4),
+      name: card.name,
+    };
+    console.log("basket Items", basketItems);
+    BASKET_SERVICE.approveBasket({ companyId: "64208d2c890cdcf8376c87a5", card: cardData, price: basketItems.totalPrice });
+  };
+
+  return (
+    <div id="payment">
+      <Cards
+        cvc={card.cvc}
+        expiry={card.expiry}
+        focused={card.focus}
+        name={card.name}
+        number={card.number}
+      />
+      <form onSubmit={formSubmitHandler}>
+        <input placeholder="name" name="name" onChange={(e) => inputOnChangeHandler(e)} onFocus={() => handleInputFocus("name")} />
+        <input placeholder="card number" name="number" onChange={(e) => inputOnChangeHandler(e)} onFocus={() => handleInputFocus("number")} />
+        <input placeholder="cvc" name="cvc" onChange={(e) => inputOnChangeHandler(e)} onFocus={() => handleInputFocus("cvc")} />
+        <input placeholder="expiry" name="expiry" onChange={(e) => inputOnChangeHandler(e)} onFocus={() => handleInputFocus("expiry")} />
+        <button type="submit">Ödemeyi Tamamla</button>
+      </form>
+    </div>
+  );
+}
