48d4b0bc3a9f703aec8c1c59fc23fbe9ea98baa0
Kaan Ersoy <kaanersoy12@gmail.com>
2023-09-17 17:36:18 +0300

    revert changes

--- server/src/services/aws.ts
+++ server/src/services/aws.ts
@@ -1,37 +1,35 @@
 // Load the AWS SDK for Node.js
-import * as AWS from "@aws-sdk/client-s3";
+import AWS from "aws-sdk";
 import { config } from "dotenv";
 import logger from "../utils/logger";
 import { DEFAULT_IMAGE_UPLOAD_MIME_TYPE } from "../constants";
 
 config();
-// AWS.config.update({ region: "eu-central-1" });
-
-const client = new AWS.S3({
+AWS.config.update({ region: "eu-central-1" });
+const s3 = new AWS.S3({
   accessKeyId: process.env.S3_ACCESS_KEY_ID,
   secretAccessKey: process.env.S3_SECRET_KEY,
-
 });
 
-// export const checkS3Connection = () => new Promise((resolve, reject) => {
-//   s3.listBuckets((err, data) => {
-//     if (err) {
-//       logger.error({ message: "Error when connection to s3 cluster", error: err });
-//       return reject(err);
-//     }
+export const checkS3Connection = () => new Promise((resolve, reject) => {
+  s3.listBuckets((err, data) => {
+    if (err) {
+      logger.error({ message: "Error when connection to s3 cluster", error: err });
+      return reject(err);
+    }
 
-//     if (data) {
-//       logger.info({ message: "Connected to s3", buckets: data.Buckets?.map((bck) => bck.Name) });
-//     }
-//     return resolve(data);
-//   });
-// });
+    if (data) {
+      logger.info({ message: "Connected to s3", buckets: data.Buckets?.map((bck) => bck.Name) });
+    }
+    return resolve(data);
+  });
+});
 
-// export const uploadPhotoToS3 = (filename, contentAsBase64, bucket = "cafefast") => s3.upload({
-//   Bucket: bucket,
-//   Key: filename,
-//   Body: contentAsBase64,
-//   ContentType: DEFAULT_IMAGE_UPLOAD_MIME_TYPE,
-// }).promise();
+export const uploadPhotoToS3 = (filename, contentAsBase64, bucket = "cafefast") => s3.upload({
+  Bucket: bucket,
+  Key: filename,
+  Body: contentAsBase64,
+  ContentType: DEFAULT_IMAGE_UPLOAD_MIME_TYPE,
+}).promise();
 
-// export const getImageFromS3 = (filename, bucket = "cafefast") => s3.getObject({ Bucket: bucket, Key: filename }).promise();
+export const getImageFromS3 = (filename, bucket = "cafefast") => s3.getObject({ Bucket: bucket, Key: filename }).promise();
