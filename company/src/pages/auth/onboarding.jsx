06798af6f17ed7e7a29b4f3d5292cc9eb7527e48
Kaan Ersoy <kaanersoy12@gmail.com>
2023-11-12 23:05:33 +0300

    add input and immer implementation to onboarding

--- company/src/pages/auth/onboarding.jsx
+++ company/src/pages/auth/onboarding.jsx
@@ -1,28 +1,34 @@
 import React, { useState, useEffect } from "react";
 import {
-  Form, Input, Button, Select,
+  Form, Button, Select,
 } from "antd";
 import { useRouter } from "next/router";
 import z, { ZodError } from "zod";
+import { useImmer } from "use-immer";
+import Input from "@/components/library/Input";
 import USER_SERVICE from "../../services/user";
 import LOCATION_SERVICE from "../../services/location";
 
 function Onboarding() {
   const router = useRouter();
-  const [companyForm, setCompanyForm] = useState({
-    name: "",
-    address: {
-      city: "",
-      district: "",
-      mailingAddress: "",
-      postalCode: "",
+
+  const [onboardingData, setOnboardingData] = useImmer({
+    user: {
+      name: "",
+      surname: "",
+      phoneNumber: "",
+    },
+    company: {
+      name: "",
+      address: {
+        city: "",
+        district: "",
+        mailingAddress: "",
+        postalCode: "",
+      },
     },
   });
-  const [userForm, setUserForm] = useState({
-    name: "",
-    surname: "",
-    phoneNumber: "",
-  });
+
   const [step, setStep] = useState("step1");
   const [city, setCity] = useState([]);
   const [district, setDistrict] = useState([]);
@@ -49,39 +55,21 @@ function Onboarding() {
     setCity(mutateCity);
   };
 
+  // TODO: City'ler çalışmıyor düzelt
   const getDistrict = async (cityID) => {
     const { data } = await LOCATION_SERVICE.getDistrict(cityID);
     const mutateDistrict = data.map((item) => ({ label: item.name, value: item.name }));
     setDistrict(mutateDistrict);
   };
 
+  useEffect(() => {
+    if (onboardingData.company.address.city) {
+      getDistrict(onboardingData.company.address.city);
+    }
+  }, [onboardingData.company.address.city]);
+
   const userFormHandler = (data) => {
-    const initialUserForm = userForm;
-    initialUserForm[data.name] = data.value;
-    setUserForm(initialUserForm);
-  };
-
-  const companyFormHandler = (data) => {
-    const initialComanyForm = companyForm;
-
-    if (data.name === "name") {
-      initialComanyForm[data.name] = data.value;
-      setCompanyForm(initialComanyForm);
-      return;
-    }
-    if (data.name === "city") {
-      getDistrict(data.value);
-      initialComanyForm.address.city = city.find((item) => item.value === data.value).label;
-      setCompanyForm(initialComanyForm);
-      return;
-    }
-    if (data.name === "postalCode") {
-      initialComanyForm.address[data.name] = Number(data.value);
-    } else {
-      initialComanyForm.address[data.name] = data.value;
-    }
-
-    setCompanyForm(initialComanyForm);
+    setOnboardingData((_onboardingData) => { _onboardingData.user[data.name] = data.value; });
   };
 
   useEffect(() => {
@@ -90,12 +78,12 @@ function Onboarding() {
 
   useEffect(() => {
     if (city.length > 0) {
-      companyFormHandler({ name: "city", value: 1 });
+      setOnboardingData((_onboardingData) => { _onboardingData.company.address.city = 1; });
     }
   }, [city]);
 
   const handleSubmit = async () => {
-    const response = await USER_SERVICE.compeleteOnboarding({ user: userForm, company: companyForm });
+    const response = await USER_SERVICE.compeleteOnboarding(onboardingData);
     if (response) {
       router.push("/");
     }
@@ -104,19 +92,19 @@ function Onboarding() {
   const formSubmitHandler = (formStep) => {
     try {
       if (formStep === "step1") {
-        stepOneVerifier.parse(userForm);
+        stepOneVerifier.parse(onboardingData.user);
         setStep("step2");
         setFormError(false);
         return;
       }
       if (formStep === "step2") {
-        stepTwoVerifier.parse(companyForm);
+        stepTwoVerifier.parse(onboardingData.company);
         setFormError(false);
         handleSubmit();
-        return;
       }
     } catch (error) {
       if (error instanceof ZodError) {
+        console.log("error :>> ", error);
         const formErrorResult = error.flatten();
         const errors = Object.keys(formErrorResult.fieldErrors).map((item) => formErrorResult.fieldErrors[item][0]);
         setFormError(errors);
@@ -126,31 +114,25 @@ function Onboarding() {
 
   const renderStep1 = () => (
     <Form>
-      <Form.Item label="Name" name="name">
-        <Input
-          name="name"
-          value={userForm.name}
-          onChange={(e) => userFormHandler({ name: "name", value: e.target.value })}
-        />
-      </Form.Item>
-      <Form.Item label="Surname" name="surname">
-        <Input
-          name="surname"
-          value={userForm.surname}
-          onChange={(e) => userFormHandler({ name: "surname", value: e.target.value })}
-        />
-      </Form.Item>
-      <Form.Item label="Phone Number" name="Phone Number">
-        <Input
-          name="Phone Number"
-          value={userForm.phoneNumber}
-          type="number"
-          onChange={(e) => userFormHandler({ name: "phoneNumber", value: Number(e.target.value) })}
-        />
-      </Form.Item>
+      <Input
+        label="İsim"
+        value={onboardingData.user.name}
+        onChange={(e) => userFormHandler({ name: "name", value: e.target.value })}
+      />
+      <Input
+        label="Soyisim"
+        value={onboardingData.user.surname}
+        onChange={(e) => userFormHandler({ name: "surname", value: e.target.value })}
+      />
+      <Input
+        label="Telefon Numarası"
+        value={onboardingData.user.phoneNumber}
+        type="number"
+        onChange={(e) => userFormHandler({ name: "phoneNumber", value: Number(e.target.value) })}
+      />
       {formError && formError.map((item) => <div>{item}</div>)}
       <Button type="primary" onClick={() => formSubmitHandler("step1")}>
-        Next
+        İleri
       </Button>
     </Form>
   );
@@ -161,49 +143,38 @@ function Onboarding() {
         city: city[0]?.label,
       }}
     >
-      <Form.Item label="City" name="city">
-        <Select
-          style={{ width: 120 }}
-          onChange={(e) => companyFormHandler({ name: "city", value: e })}
-          options={city}
-          name="city"
-          value={companyForm.address.city}
-        />
-
-      </Form.Item>
-      <Form.Item label="district" name="district">
-        <Select
-          disabled={district.length === 0}
-          style={{ width: 120 }}
-          onChange={(e) => companyFormHandler({ name: "district", value: e })}
-          options={district}
-          name="district"
-          value={companyForm.address.city}
-        />
-      </Form.Item>
-      <Form.Item label="Address" name="mailingAddress">
-        <Input
-          name="mailingAddress"
-          value={companyForm.address.mailingAddress}
-          onChange={(e) => companyFormHandler({ name: "mailingAddress", value: e.target.value })}
-        />
-      </Form.Item>
-      <Form.Item label="Postal Code" name="postalCode">
-        <Input
-          type="number"
-          name="postalCode"
-          value={companyForm.address.postalCode}
-          onChange={(e) => companyFormHandler({ name: "postalCode", value: e.target.value })}
-        />
-      </Form.Item>
-      <Form.Item label="Comapny Name" name="companyName">
-        <Input
-          name="company"
-          value={companyForm.name}
-          onChange={(e) => companyFormHandler({ name: "name", value: e.target.value })}
-        />
-
-      </Form.Item>
+      <Input
+        label="Şirket Adı"
+        description="Şirketinizin adını giriniz"
+        value={onboardingData.company.name}
+        onChange={(e) => setOnboardingData((_onboardingData) => { _onboardingData.company.name = e.target.value; })}
+      />
+      <Select
+        style={{ width: 120 }}
+        onChange={(e) => setOnboardingData((_onboardingData) => { _onboardingData.company.address.city = city[e].name; })}
+        options={city}
+        value={onboardingData.company.address.city}
+      />
+      <Select
+        disabled={district.length === 0}
+        style={{ width: 120 }}
+        onChange={(e) => setOnboardingData((_onboardingData) => { _onboardingData.company.address.district = e.name; })}
+        options={district}
+        value={onboardingData.company.address.district}
+      />
+      <Input
+        label="Açık Adres"
+        description="Şirketinizin açık adresini giriniz"
+        value={onboardingData.company.address.mailingAddress}
+        onChange={(e) => setOnboardingData((_onboardingData) => { _onboardingData.company.address.mailingAddress = e.target.value; })}
+      />
+      <Input
+        label="Posta Kodu"
+        description="Şirketinizin posta kodunu giriniz"
+        type="number"
+        value={onboardingData.company.address.postalCode}
+        onChange={(e) => setOnboardingData((_onboardingData) => { _onboardingData.company.address.postalCode = Number(e.target.value); })}
+      />
       {formError && formError.map((item) => <div>{item}</div>)}
       <Button type="primary" onClick={() => formSubmitHandler("step2")}>
         Submit
