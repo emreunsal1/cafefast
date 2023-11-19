04709d4acd55b3789a14eb3131457e8dc5696ec9
Kaan Ersoy <kaanersoy12@gmail.com>
2023-11-19 23:00:27 +0300

    add localstorage to side bar

--- company/next.config.js
+++ company/next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  poweredByHeader: false,
   webpackDevMiddleware: (config) => {
     // Solve compiling problem via vagrant
     config.watchOptions = {
--- company/src/components/SideBar.jsx
+++ company/src/components/SideBar.jsx
@@ -1,14 +1,13 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import classNames from "classnames";
 import AUTH_SERVICE from "@/services/auth";
+import { STORAGE } from "@/utils/browserStorage";
 
+// TODO: Buradaki localstorage transition problemini çöz.
 export default function SideBar() {
   const router = useRouter();
-  const logout = async () => {
-    await AUTH_SERVICE.logout();
-  };
-  const [isOpened, setIsOpened] = useState(true);
+  const [isOpened, setIsOpened] = useState(false);
 
   const MenuItems = [
     { key: "Anasayfa", route: "/", icon: "home" },
@@ -19,20 +18,32 @@ export default function SideBar() {
     { key: "Table", route: "/table", icon: "qr-icon" },
     { key: "Campain", route: "/campaings", icon: "campain" },
     {
-      key: "Logout", route: "/auth/login", clicked: logout, icon: "exit",
+      key: "Logout", route: "/auth/login", clicked: AUTH_SERVICE.logout, icon: "exit",
     },
   ];
 
+  const openHandler = () => {
+    STORAGE.setLocal("sideMenuOpened", !isOpened);
+    setIsOpened(!isOpened);
+  };
+
+  useEffect(() => {
+    setIsOpened(STORAGE.getLocal("sideMenuOpened") === "true");
+  }, []);
+
+  const placeholderClassname = classNames("sidebar-placeholder", { closed: !isOpened });
+  const sidebarClassname = classNames("side-bar", { closed: !isOpened });
+
   return (
     <>
-      <div className={`sidebar-placeholder ${isOpened ? "" : "closed"}`} />
-      <div id="Menu" className={`side-bar ${isOpened ? "" : "closed"}`}>
+      <div className={placeholderClassname} />
+      <div id="Menu" className={sidebarClassname}>
         <div className="container">
           <div className="header">
-            <div className="comapny-place">
-              {isOpened ? "CAFE FAST" : "CF" }
+            <div className="company-place">
+              {isOpened ? "CAFE FAST" : "C" }
             </div>
-            <div className="opened-button" onClick={() => setIsOpened(!isOpened)}>
+            <div className="opened-button" onClick={openHandler}>
               <i className="icon icon-sidemenu" />
             </div>
           </div>
